e72d4c3a303c6751052dc504bc1ea3a71cbefe38
ianchou <ianchou@users.noreply.github.com>
2010-06-26 05:21:42 +0000

    No commit message

--- YoutubeTest.java
+++ YoutubeTest.java
@@ -6,6 +6,7 @@ import java.net.*;
 public class YoutubeTest{
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
+		System.out.println("please input youtube URL:");
 		while(in.hasNext()){
 			try{
 				BufferedInputStream input= new BufferedInputStream(new URL(new YoutubeParser(in.nextLine()).getURL()).openStream());
@@ -15,6 +16,7 @@ public class YoutubeTest{
 				while((count = input.read(data,0,1024))!=-1){
 					output.write(data,0,count);
 				}
+				System.out.println("done");
 				input.close();
 				output.close();
 			}catch(Exception e){
--- ianchou/run.sh
+++
@@ -1,2 +0,0 @@
-java -cp ../lib/htmlparser.jar:../lib/htmllexer.jar:../lib/filterbuilder.jar:../lib/sitecapturer.jar:../lib/thumbelina.jar:. $1 
-
