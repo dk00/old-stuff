8306adb1e169465055f6e118e0805507c0710db7
ianchou <ianchou@users.noreply.github.com>
2010-06-26 05:15:36 +0000

    No commit message

---
+++ YoutubeTest.java
@@ -0,0 +1,25 @@
+import ianchou.*;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class YoutubeTest{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		while(in.hasNext()){
+			try{
+				BufferedInputStream input= new BufferedInputStream(new URL(new YoutubeParser(in.nextLine()).getURL()).openStream());
+				BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream("test.flv"));
+				byte[] data = new byte[1024];
+				int count;
+				while((count = input.read(data,0,1024))!=-1){
+					output.write(data,0,count);
+				}
+				input.close();
+				output.close();
+			}catch(Exception e){
+				System.out.println(e);
+			}
+		}
+	}
+}
