96a6a19f86f62bd7387c3422ef1065fa9576657d
hank5925 <hank5925@users.noreply.github.com>
2010-06-26 06:47:02 +0000

    123

--- Download.java
+++ Download.java
@@ -6,16 +6,23 @@ import java.util.*;
 public class Download{
 	public static void main(String[] args){
 		try{
-			String s;
-			BufferedInputStream input= new BufferedInputStream(
-					new URL("http", args[0], 80, args[1]).openStream());
-			s = args[1].substring(args[1].lastIndexOf("/")+1);
-			BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(s));
+			//init
+			String s, t;
 			byte[] data = new byte[1024];
-			int count;
+			int count, idx;
+			//alloc
+			idx = args[0].indexOf("/");
+			s = args[0].substring(0, idx);
+			t = args[0].substring(idx);
+			BufferedInputStream input = new BufferedInputStream(
+					new URL("http", s, 80, t).openStream());
+			s = t.substring(t.lastIndexOf("/")+1);
+			BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(s));
+			//r/w
 			while((count = input.read(data,0,1024))!=-1){
 				output.write(data,0,count);
 			}
+			//close
 			input.close();
 			output.close();
 		}catch(Exception e){
--- Update.java
+++ Update.java
@@ -7,7 +7,7 @@ import java.lang.*;
 public class Update{
 	public static void main(String args[]){
 		final int PORT = 80;
-		if (args.length<2){
+		if (args.length<3){
 			System.exit(1);
 		}
 		Socket socket;
@@ -16,16 +16,9 @@ public class Update{
 		OutputStream OS;
 		BufferedReader BR;
 		PrintStream PS;
-			//Date date = new Date();
-			//Locale locale = new Locale("en");
-			//DateFormat DF = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss", locale);
 		try{
 			// build client socket
 			socket=new Socket(args[0], PORT);
-			// notify user
-			System.out.println("client ok");
-			System.out.println("************************************************");
-			System.out.println("");
 			// build socket stream
 			IS=socket.getInputStream();
 			OS=socket.getOutputStream();
@@ -33,22 +26,20 @@ public class Update{
 			BR = new BufferedReader(new InputStreamReader(IS));
 			PS = new PrintStream(OS);
 			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			
 			// do something
-			s = "GET " + args[1] + " HTTP/1.0";
-			PS.println(s);
-				//s =	"If-Modified-Since: " + DF.format(date) + " GMT";	//current time
-			s =	"If-Modified-Since: Fri, 25 Jun 2010 16:33:36 GMT";
-			PS.println(s);
-			s = "";
-			PS.println(s);
+			s = "GET " + args[1] + " HTTP/1.0";		PS.println(s);
+			s =	"If-Modified-Since: " + args[2];	PS.println(s);
+				//Fri, 25 Jun 2010 16:33:36 GMT
+			s = "";									PS.println(s);
 			s = BR.readLine();
 			t = s.substring(9, 12);
 			if (t.compareTo("302") == 0){
-				System.out.println("302 Found");
+				//System.out.println("302 Found");
 			} else if (t.compareTo("304") == 0){
-				System.out.println("304 Not Modified");
+				//System.out.println("304 Not Modified");
 			} else{
-				System.out.println(t);
+				//System.out.println(t);
 			}
 
 			// close
