ea71a0d929ef93ba88d1fb925392d7f6fb42697c
hank5925 <hank5925@users.noreply.github.com>
2010-06-27 14:39:03 +0000

    updatepanel

--- Update.java
+++ Update.java
@@ -1,14 +1,86 @@
-//Usage:	java Update [linkAddr] [lastCheckDate]
-//			(ex. java Download w.csie.org/~b96070/hell.jpg Fri,\ 25\ Jun\ 2010\ 16:33:36\ GMT)
-
 import java.net.*;
 import java.io.*;
 import java.lang.*;
-//import java.text.*;
-//import java.util.*;
+import java.text.*;
+import java.util.*;
 
 public class Update{
-	public static int search(String addr, String time){
+	private static String getTime(){
+		SimpleDateFormat df = new SimpleDateFormat(
+			"EEE, d MMM yyyy HH:mm:ss ", new Locale("en"));
+		return df.format(new Date()) + "GMT";
+	}
+	private static void download(String arg){
+		try{
+			//init
+			String s, t;
+			byte[] data = new byte[1024];
+			int count, idx;
+			//alloc
+			idx = arg.indexOf("/");
+			s = arg.substring(0, idx);
+			t = arg.substring(idx);
+			BufferedInputStream input = new BufferedInputStream(
+					new URL("http", s, 80, t).openStream());
+			s = t.substring(t.lastIndexOf("/")+1);
+			BufferedOutputStream output = new BufferedOutputStream(
+					new FileOutputStream("tmp/" + s));
+			//r/w
+			while((count = input.read(data,0,1024))!=-1){
+				output.write(data,0,count);
+			}
+			//close
+			input.close();
+			output.close();
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	public static int search(String addr) throws IOException{
+		int check = 0;
+		String s, t;
+		BufferedReader in = new BufferedReader(
+								new FileReader("mydata"));
+		BufferedWriter out = new BufferedWriter(
+								new FileWriter("tmp/mydata"));
+		while ( (s = in.readLine()) != null ) {
+			if ( s.compareTo(addr) == 0 ){
+				check = 1;
+				t = in.readLine();
+				switch (update(addr, t)){
+					case 302:	break;	//found
+					case 304://not modified
+					case 200://ok
+						Update.download(addr);
+						break;
+					case 404:	break;	//not found
+					default:	break;
+				}
+				out.write(s);	out.newLine();
+				t = Update.getTime();
+				out.write(t);	out.newLine();
+				continue;
+			}
+			out.write(s);	out.newLine();
+		}
+		if (check == 0){
+			t = Update.getTime();
+			switch (update(addr, t)){
+				case 302:	break;	//found
+				case 304://not modified
+				case 200://ok
+							Update.download(addr);
+							break;
+				case 404:	break;	//not found
+				default:	break;
+			}
+			out.write(addr);	out.newLine();
+			out.write(t);		out.newLine();
+		}
+		in.close();		out.close();
+		return 0;
+	}
+	private static int update(String addr, String time){
 		// alloc
 		final int PORT = 80;
 		int idx;
@@ -49,10 +121,10 @@ public class Update{
 		}
 		return -1;
 	}
-
-	public static void main(String args[]){
-		//int i = Update.search("w.csie.org/~b96070/hell.jpg", "Fri, 25 Jun 2010 16:33:36 GMT");
-		int i = Update.search(args[0], args[1]);
-		System.out.println(i);
+	public Update(String s) throws IOException{
+		Update.search(s);
+	}
+	public static void main(String args[]) throws IOException{
+		Update.search(args[0]);
 	}
 }
