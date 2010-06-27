3fe466dc8991322923eef3e583dd316a0ad7c947
ianchou <ianchou@users.noreply.github.com>
2010-06-27 08:19:29 +0000

    No commit message

---
+++ ianchou/ImageInfo.java
@@ -0,0 +1,12 @@
+package ianchou;
+
+public class ImageInfo{
+	public String url,title;
+	public int width,height;
+	public ImageInfo(String u, String t, int w, int h){
+		url = u; 
+		title = t;
+		width = w;
+		height = h;
+	}
+}
